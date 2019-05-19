17323865bc43b42e299a20ec10aa4d06c8815bdd
yusuf demirkoparan <ydemirkoparan@gmail.com>
2019-05-19 04:12:25 +0300

    added SearchBar

--- GoogleBooks(MVVM)/Scenes/SearchBooks/SearchBooksViewController.swift
+++ GoogleBooks(MVVM)/Scenes/SearchBooks/SearchBooksViewController.swift
@@ -8,35 +8,47 @@
 
 import UIKit
 
-class SearchBooksViewController : UIViewController {
+class SearchBooksViewController : UITableViewController {
     
     //MARK: - Properties
-    
+    let searchController = UISearchController(searchResultsController: nil)
+    let cellID = "cell"
     // MARK: - Init
     override func viewDidLoad() {
         super.viewDidLoad()
         title = "test"
         setupNavBar()
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: cellID)
         
     }
     func setupNavBar() {
+        
         navigationController?.navigationBar.prefersLargeTitles = true
         navigationItem.largeTitleDisplayMode = .always
-        let searchController = UISearchController(searchResultsController: nil)
-        searchController.searchResultsUpdater = self
+      
+        
         searchController.obscuresBackgroundDuringPresentation = false
+        searchController.searchBar.delegate = self
         searchController.searchBar.placeholder = "Search Candies"
         navigationItem.searchController = searchController
         definesPresentationContext = true
         navigationItem.hidesSearchBarWhenScrolling = false
+//        tableView.tableHeaderView = searchController.searchBar
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellID, for: indexPath)
+        return cell
     }
     // MARK: - Handlers
 }
 
-extension SearchBooksViewController : UISearchResultsUpdating {
-    func updateSearchResults(for searchController: UISearchController) {
-        guard let text = searchController.searchBar.text else { return }
-        print(text)
+extension SearchBooksViewController : UISearchBarDelegate {
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        print("asdas")
     }
-
+    
 }
